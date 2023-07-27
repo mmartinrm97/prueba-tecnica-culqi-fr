798faf942d4bd0d33b492257d57f2dd085b2aa01
Miguel <mmartinrm3@gmail.com>
2023-07-27 15:07:51 -0500

    :adhesive_bandage: Fix errors on pagination

--- src/admin/empleados/components/PaginationNumbers.vue
+++ src/admin/empleados/components/PaginationNumbers.vue
@@ -1,60 +1,47 @@
 <template>
-  <div class="flex items-center justify-between mt-10 space-x-2">
-    <button
-      :class="[currentPage === 1 ? 'cursor-not-allowed' : '']"
-      :disabled="currentPage === 1"
-      @click="getPage(currentPage - 1)"
-    >
-      Anterior
-    </button>
-
-    <button
-      v-for="number of totalPageNumbers"
-      :key="number"
-      :class="[currentPage === number ? 'bg-primary text-white' : 'bg-white']"
-      @click="getPage(number)"
-    >
-      {{ number }}
-    </button>
-
-    <button
-      :class="[currentPage === totalPages ? 'cursor-not-allowed' : '']"
-      :disabled="currentPage === totalPages"
-      @click="getPage(currentPage + 1)"
-    >
-      Siguiente
-    </button>
-  </div>
-
   <!-- paginator -->
-<!--<div class="flex justify-between">-->
-<!--  <div class="flex justify-center gap-x-2 pt-8">-->
-<!--    <button class="flex h-8 w-8 items-center justify-center">-->
-<!--      <ChevronLeftIcon-->
-<!--          class="h-6 w-6 to-gray-800 stroke-current hover:text-indigo-600"-->
-<!--      />-->
-<!--    </button>-->
-<!--    <button-->
-<!--        v-for="i in 6"-->
-<!--        class="flex h-8 w-8 items-center justify-center rounded-full font-medium"-->
-<!--        :class="-->
-<!--            i === 1-->
-<!--              ? 'bg-gray-800 text-white'-->
-<!--              : 'text-gray-400 hover:text-indigo-600'-->
-<!--          "-->
-<!--    >-->
-<!--      {{ i }}-->
-<!--    </button>-->
-<!--    <button class="flex h-8 w-8 items-center justify-center">-->
-<!--      <ChevronRightIcon-->
-<!--          class="h-6 w-6 to-gray-800 stroke-current hover:text-indigo-600"-->
-<!--      />-->
-<!--    </button>-->
-<!--  </div>-->
-<!--  <div class="flex justify-center">-->
-<!--    Mostrando 1 a 8 de 50 registros-->
-<!--  </div>-->
-<!--</div>-->
+  <div class="flex justify-between items-center">
+    <div class="flex justify-center gap-x-2 pt-8">
+      <button
+        :class="[currentPage === 1 ? 'cursor-not-allowed' : '']"
+        :disabled="currentPage === 1"
+        class="flex h-8 w-8 items-center justify-center border-2 rounded-lg mr-6"
+        @click="getPage(currentPage - 1)"
+      >
+        <ChevronLeftIcon
+          :class="[currentPage === 1 ? 'text-gray-200' : 'text-secondary hover:text-secondary']"
+          class="h-6 w-6"
+        />
+      </button>
+      <button
+        v-for="number of totalPageNumbers"
+        :key="number"
+        :class="
+          currentPage === number
+            ? 'bg-gray-100 text-secondary'
+            : 'text-gray-400 hover:text-secondary'
+        "
+        class="flex h-8 w-8 items-center justify-center rounded-lg font-medium"
+        @click="getPage(number)"
+      >
+        {{ number }}
+      </button>
+      <button
+        :class="[currentPage === totalPages ? 'cursor-not-allowed' : '']"
+        :disabled="currentPage === totalPages"
+        class="flex h-8 w-8 items-center border-2 rounded-lg justify-center ml-6"
+        @click="getPage(currentPage + 1)"
+      >
+        <ChevronRightIcon
+          :class="[
+            currentPage === totalPages ? 'text-gray-200' : 'text-secondary hover:text-secondary'
+          ]"
+          class="h-6 w-6"
+        />
+      </button>
+    </div>
+    <div class="flex justify-center">Mostrando x registros</div>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -62,11 +49,9 @@ import useEmpleados from '@/admin/empleados/composables/useEmpleados'
 import ChevronLeftIcon from '@/assets/icons/chevron-left.svg'
 import ChevronRightIcon from '@/assets/icons/chevron-right.svg'
 
-const { getPage, totalPageNumbers, currentPage, totalPages } = useEmpleados()
 
+const { getPage, totalPageNumbers, currentPage, totalPages } = useEmpleados()
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
